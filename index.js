18588eb95ab56e8ef64d389414e7a020cc93d91a
Alisa Chernobrovetc <alisa_c@mail.ua>
2017-02-20 23:30:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+function getTotalAmountForProducts(products, callback) {
+  let totalPrice = 0;
+ 
+  products.forEach(product => {
+    totalPrice = callback(totalPrice, product);
+  });
+ 
+  return totalPrice;
+}
+ 
+function callback(totalPrice, product) {
+  if (product.discount >= .5) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+ 
+console.log(getTotalAmountForProducts(products, callback)); // prints 25.5
